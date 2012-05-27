b3f08dcdb0c2f75a2ec1750ad75dacf1a1cf82b9
Jan Lofberg <chenebu@gmail.com>
2012-05-27 09:22:52 +0200

    new line before making entry

--- c-vhost.sh
+++ c-vhost.sh
@@ -17,13 +17,20 @@ vhost="<VirtualHost *:80>
 </VirtualHost>
 "
 
-
+# To make sure we get a new line before input
+echo ' ' >> /etc/hosts
+# Add the virtual server nam to hosts
 echo "127.0.0.1" $1 >> /etc/hosts
 
+# To make sure we get a new line before input
+echo ' ' >> /private/etc/apache2/extra/httpd-vhosts.conf
+# Add the irtual server config to httpd-vhosts.conf
 echo "${vhost}" >> /private/etc/apache2/extra/httpd-vhosts.conf
 
+# Restart the webserver
 `apachectl restart`
 
+# Confirm the new settings
 echo "
 created new virtual host $1 in ${path}
 "
